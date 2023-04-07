8766624987d18ddc2c41f57ee71329e45e612289
PhamNhatSang20194657 <sang.pn194657@gmail.com>
2023-04-07 11:48:39 +0700

    update

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/jobtitleSA/history/JobHistoryRepository.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/jobtitleSA/history/JobHistoryRepository.java
@@ -1,25 +1,5 @@
 package nts.uk.ctx.bs.employee.dom.jobtitleSA.history;
 
-import nts.arc.time.GeneralDate;
-
 public interface JobHistoryRepository {
-	void addHistory(JobHistory jobhistory);
 
-	/**
-	 * Update history.
-	 *
-	 * @param companyId the company id
-	 * @param historyId the history id
-	 * @param endĐate the end date
-	 */
-	void updateHistory(String historyId, GeneralDate endDate);
-	
-	/**
-	 * Update lastest history.
-	 *
-	 * @param companyId the company id
-	 * @param jobTitleId the job title id
-	 * @param endĐate the end date
-	 */
-	void updateLastestHistory(String jobInforId, GeneralDate endDate);
 }
